4c262d5efc90645280ac0ee92bae102f34eab05a
Felipe Carneiro <felipecarneiro@duck.com>
2023-05-03 22:59:13 -0300

    End of the configuration of Entity Framework

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace app_web_backend_5.Models {
+
+    // deve ter uma herança "DbContext"
+    public class ApplicationDbContext : DbContext {
+
+    }
+}
--- Models/veiculo.cs
+++ Models/veiculo.cs
@@ -1,4 +1,18 @@
-﻿namespace app_web_backend_5.Models {
-    public class Class {
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_web_backend_5.Models {
+
+    [Table("Veículos")]
+    public class veiculo {
+
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Obrigadório informar o <nome>")]
+        public string nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigadório informar o <placa>")]
+        public int placa { get; set; }
     }
 }
