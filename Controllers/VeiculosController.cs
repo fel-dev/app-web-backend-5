65bf8a7aee600ac1d6fa07f375697f137ae880ba
Felipe Carneiro <felipecarneiro@duck.com>
2023-05-04 18:08:44 -0300

    Feat: criou VeiculosController

---
+++ Controllers/VeiculosController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using app_web_backend_5.Models;
+
+namespace app_web_backend_5.Controllers
+{
+    public class VeiculoeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VeiculoeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Veiculoe
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Veiculos.ToListAsync());
+        }
+
+        // GET: Veiculoe/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var veiculo = await _context.Veiculos
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(veiculo);
+        }
+
+        // GET: Veiculoe/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Veiculoe/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,nome,placa")] Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(veiculo);
+        }
+
+        // GET: Veiculoe/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var veiculo = await _context.Veiculos.FindAsync(id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+            return View(veiculo);
+        }
+
+        // POST: Veiculoe/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,nome,placa")] Veiculo veiculo)
+        {
+            if (id != veiculo.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(veiculo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VeiculoExists(veiculo.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(veiculo);
+        }
+
+        // GET: Veiculoe/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var veiculo = await _context.Veiculos
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(veiculo);
+        }
+
+        // POST: Veiculoe/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var veiculo = await _context.Veiculos.FindAsync(id);
+            _context.Veiculos.Remove(veiculo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VeiculoExists(int id)
+        {
+            return _context.Veiculos.Any(e => e.ID == id);
+        }
+    }
+}
